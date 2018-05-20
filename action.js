def371655229ac49120580a502ce87c3ccebae2a
Marcin <m.kotlinski@protonmail.com>
2018-05-20 17:06:07 +0200

    updated gallery size responsive

--- action.js
+++ action.js
@@ -1,5 +1,15 @@
-var clicks = 0;
-function onClick() {
-    clicks += 1;
-    document.getElementById("counter").innerHTML = clicks;
-};
+var clicks9 = 0;
+var dropmenu = document.querySelector('#menu ul');
+
+window.onload = function () {
+        document.getElementById("heart1").addEventListener("click", function(event){
+            event.preventDefault();
+            clicks9 += 1;
+            document.getElementById("krum").innerHTML = clicks9;
+        });
+
+        document.querySelector('.drop-button').addEventListener("click", function(event){
+            event.preventDefault();
+            dropmenu.classList.toggle('active');
+        });
+    };
